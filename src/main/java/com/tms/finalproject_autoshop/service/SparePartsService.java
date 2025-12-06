19fcca6c53d892a84cdf625f151efd37db39b5c2
EvgeniyGorbeko1 <evgeniy.gorbenko2@gmail.com>
2025-12-06 21:24:25 +0300

    1

--- src/main/java/com/tms/finalproject_autoshop/service/SparePartsService.java
+++ src/main/java/com/tms/finalproject_autoshop/service/SparePartsService.java
@@ -1,162 +1,63 @@
 package com.tms.finalproject_autoshop.service;
 
-import com.tms.finalproject_autoshop.model.*;
+import com.tms.finalproject_autoshop.model.SpareParts;
+import com.tms.finalproject_autoshop.model.User;
 import com.tms.finalproject_autoshop.model.dto.PartDto;
-import com.tms.finalproject_autoshop.repository.CatalogRepository;
-import com.tms.finalproject_autoshop.repository.OilPartsRepository;
+import com.tms.finalproject_autoshop.model.dto.UserCreateDto;
 import com.tms.finalproject_autoshop.repository.SparePartsRepository;
-import com.tms.finalproject_autoshop.repository.ServicePartsRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Service
 public class SparePartsService {
     private final SparePartsRepository sparePartsRepository;
-    private final ServicePartsRepository servicePartsRepository;
-    private final OilPartsRepository oilPartsRepository;
-    private final CatalogRepository catalogRepository;
 
-
-    public SparePartsService(SparePartsRepository sparePartsRepository,
-                             ServicePartsRepository servicePartsRepository,
-                             OilPartsRepository oilPartsRepository,
-                             CatalogRepository catalogRepository) {
+    public SparePartsService(SparePartsRepository sparePartsRepository) {
         this.sparePartsRepository = sparePartsRepository;
-        this.servicePartsRepository = servicePartsRepository;
-        this.oilPartsRepository = oilPartsRepository;
-        this.catalogRepository = catalogRepository;
     }
 
-    public Boolean updateSparePart(PartDto partDto) {
+    public Boolean createPart(PartDto partDto) {
         try {
             SpareParts newSparePart = new SpareParts();
             newSparePart.setName(partDto.getName());
-            newSparePart.setBrand(partDto.getBrand());
+            newSparePart.setCategory(partDto.getCategory());
             newSparePart.setPrice(partDto.getPrice());
             newSparePart.setImage(partDto.getImage());
-            newSparePart.setArticle(partDto.getArticle());
-            newSparePart.setCarBrand(partDto.getCarBrand());
-            sparePartsRepository.save(newSparePart);
+            newSparePart.setDescription(partDto.getDescription());
         } catch (Exception ex) {
             System.out.println("Error in saving user: " + ex.getMessage());
         }
         return true;
     }
 
-    public Boolean updateServicePart(PartDto partDto) {
-        try {
-            ServiceParts newServicePart = new ServiceParts();
-            newServicePart.setName(partDto.getName());
-            newServicePart.setCarBrand(partDto.getCarBrand());
-            newServicePart.setPrice(partDto.getPrice());
-            newServicePart.setImage(partDto.getImage());
-            newServicePart.setArticle(partDto.getArticle());
-            newServicePart.setBrand(partDto.getBrand());
-            servicePartsRepository.save(newServicePart);
-        } catch (Exception ex) {
-            System.out.println("Error in saving user: " + ex.getMessage());
-        }
-        return true;
-    }
-
-    public Boolean updateOilPart(PartDto partDto) {
-        try {
-            OilParts newOilPart = new OilParts();
-            newOilPart.setName(partDto.getName());
-            newOilPart.setBrand(partDto.getBrand());
-            newOilPart.setPrice(partDto.getPrice());
-            newOilPart.setImage(partDto.getImage());
-            newOilPart.setArticle(partDto.getArticle());
-            oilPartsRepository.save(newOilPart);
-        } catch (Exception ex) {
-            System.out.println("Error in saving user: " + ex.getMessage());
-        }
-        return true;
-    }
-
-
-    public Object getProductById(String type, Long id) {
-        if (ProductTypes.SPARE_PART.equals(type)) {
-            Optional<SpareParts> spareParts = sparePartsRepository.findById(id);
-        } else if (ProductTypes.OIL_PART.equals(type)) {
-            Optional<OilParts> oilParts = oilPartsRepository.findById(id);
-        } else if (ProductTypes.SERVICE_PART.equals(type)) {
-            Optional<ServiceParts> serviceParts = servicePartsRepository.findById(id);
-        } else {
-            return Optional.empty();
-        }
-    return null;
-    }
-
-
-    public Object createProduct(String type, SpareParts spareParts, OilParts oilParts, ServiceParts serviceParts) {
-        if (ProductTypes.SPARE_PART.equals(type)) {
-            return sparePartsRepository.save(spareParts);
-        } else if (ProductTypes.OIL_PART.equals(type)) {
-            return oilPartsRepository.save(oilParts);
-        } else if (ProductTypes.SERVICE_PART.equals(type)) {
-            return servicePartsRepository.save(serviceParts);
-        } else {
-            return Optional.empty();
-        }
+    public Optional<SpareParts> getPartById(Long id) {
+        return sparePartsRepository.findById(id);
 
     }
 
-    public List<?> getAllProducts(String type) {
-        if (ProductTypes.SPARE_PART.equals(type)) {
-            return sparePartsRepository.findAll();
-        } else if (ProductTypes.OIL_PART.equals(type)) {
-            return oilPartsRepository.findAll();
-        } else if (ProductTypes.SERVICE_PART.equals(type)) {
-            return servicePartsRepository.findAll();
+    public Optional<SpareParts> updateSpareParts(SpareParts spareParts) {
+        Optional<SpareParts> updatePartDB = getPartById(spareParts.getId());
+        if (updatePartDB.isPresent()) {
+            return Optional.of(sparePartsRepository.save(spareParts));
         } else {
             throw new NullPointerException();
         }
     }
 
-    public void deleteProductById(String type, Long id) {
-        if (ProductTypes.SPARE_PART.equals(type)) {
+    public List<SpareParts> getAllSpareParts() {
+        return sparePartsRepository.findAll();
+    }
+
+    public Boolean deleteSparePart(Long id) {
+        if(sparePartsRepository.existsById(id)) {
             sparePartsRepository.deleteById(id);
-        } else if (ProductTypes.OIL_PART.equals(type)) {
-            oilPartsRepository.deleteById(id);
-        } else if (ProductTypes.SERVICE_PART.equals(type)) {
-            servicePartsRepository.deleteById(id);
-        } else {
-            throw new NullPointerException();
-        }
-    }
-
-    public List<Catalog> getAllCatalog() {
-        return catalogRepository.findAll();
-    }
-
-    public Optional<Catalog> getCatalogById(Long id) {
-        return catalogRepository.findById(id);
-    }
-
-    public Catalog createCatalog(Catalog catalog) {
-        catalogRepository.save(catalog);
-        return catalog;
-    }
-
-    public Optional<Catalog> deleteCatalogById(Long id) {
-        catalogRepository.deleteById(id);
-        return Optional.empty();
-    }
-
-    public Catalog updateCatalog(Catalog catalog, Long id) {
-        Catalog newCatalog = new Catalog();
-        if(catalogRepository.findById(id).isPresent()) {
-            newCatalog.setName(catalog.getName());
-            newCatalog.setDescription(catalog.getDescription());
-            newCatalog.setImage(catalog.getImage());
+            return true;
         } else{
-            throw new RuntimeException("Catalog id not found");
+            throw new NullPointerException(); //TODO: exception
         }
-        return catalogRepository.save(newCatalog);
     }
-
-
 }
