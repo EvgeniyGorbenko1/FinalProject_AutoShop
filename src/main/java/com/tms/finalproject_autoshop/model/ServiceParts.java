cef5699c518c24cbb93ae608a9d1dfe90a5200d9
EvgeniyGorbeko1 <evgeniy.gorbenko2@gmail.com>
2025-12-09 14:28:57 +0300

    Models

--- src/main/java/com/tms/finalproject_autoshop/model/ServiceParts.java
+++ src/main/java/com/tms/finalproject_autoshop/model/ServiceParts.java
@@ -3,34 +3,30 @@ package com.tms.finalproject_autoshop.model;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 
-@Data
+//@Data
+@Scope("prototype")
 @NoArgsConstructor
 @Entity(name = "service_parts")
-@EqualsAndHashCode(exclude = {"catalog"})
-@ToString(exclude = {"catalog"})
-public class ServiceParts {
-    @Id
-    @SequenceGenerator(name = "service_parts_generator", sequenceName = "service_parts_id_seq", allocationSize = 1)
-    @GeneratedValue(generator = "service_parts_generator")
-    private Long id;
-    private String name;
-    private String brand;
-    private Double price;
-    private String image;
-    private Integer article;
+//@EqualsAndHashCode(exclude = {"catalog"})
+//@ToString(exclude = {"catalog"})
+public class ServiceParts extends ProductAbstract {
+//    @Id
+//    @SequenceGenerator(name = "service_parts_generator", sequenceName = "service_parts_id_seq", allocationSize = 1)
+//    @GeneratedValue(generator = "service_parts_generator")
+//    private Long id;
+    @Getter
+    @Setter
     private String carBrand;
-    @JsonBackReference
-    @ManyToOne
-    @JoinColumn(name = "catalog_id")
-    private Catalog catalog;
+//    @JsonBackReference
+//    @ManyToOne
+//    @JoinColumn(name = "catalog_id")
+//    private Catalog catalog;
 
 
 }
--- src/main/java/com/tms/finalproject_autoshop/model/SpareParts.java
+++ src/main/java/com/tms/finalproject_autoshop/model/SpareParts.java
@@ -3,34 +3,29 @@ package com.tms.finalproject_autoshop.model;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 
-@Data
+//@Data
 @NoArgsConstructor
-@Entity(name = "original_parts")
-@EqualsAndHashCode(exclude = {"catalog"})
-@ToString(exclude = {"catalog"})
-public class SpareParts {
-    @Id
-    @SequenceGenerator(name = "original_parts_generator", sequenceName = "original_parts_id_seq", allocationSize = 1)
-    @GeneratedValue(generator = "original_parts_generator")
-    private Long id;
-    private String name;
-    private String brand;
-    private Double price;
-    private String image;
-    private Integer article;
+@Entity(name = "spare_parts")
+//@EqualsAndHashCode(exclude = {"catalog"})
+//@ToString(exclude = {"catalog"})
+public class SpareParts extends ProductAbstract {
+//    @Id
+//    @SequenceGenerator(name = "original_parts_generator", sequenceName = "original_parts_id_seq", allocationSize = 1)
+//    @GeneratedValue(generator = "original_parts_generator")
+//    private Long id;
+    @Getter
+    @Setter
     private String carBrand;
-    @JsonBackReference
-    @ManyToOne
-    @JoinColumn(name = "catalog_id")
-    private Catalog catalog;
+
+    //    @JsonBackReference
+//    @ManyToOne
+//    @JoinColumn(name = "catalog_id")
+//    private Catalog catalog;
 
 
 }
