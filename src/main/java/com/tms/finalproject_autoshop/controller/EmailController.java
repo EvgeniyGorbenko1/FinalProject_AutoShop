f8e1b3746d504ee9b61649ae299a7093bb4cfc24
EvgeniyGorbeko1 <evgeniy.gorbenko2@gmail.com>
2025-12-21 23:05:16 +0300

    email notifications fix + works

--- src/main/java/com/tms/finalproject_autoshop/controller/EmailController.java
+++ src/main/java/com/tms/finalproject_autoshop/controller/EmailController.java
@@ -1,26 +1,26 @@
-//package com.tms.finalproject_autoshop.controller;
-//
-//import com.tms.finalproject_autoshop.service.EmailService;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class EmailController {
-//    private final EmailService emailService;
-//
-//    public EmailController(EmailService emailService) {
-//        this.emailService = emailService;
-//    }
-//
-//    @GetMapping("/notify")
-//    public ResponseEntity<String> notifyEmail() {
-//        emailService.sendEmail("",
-//                "",
-//                "");
-//        return new ResponseEntity<>("Email sent successfully", HttpStatus.OK);
-//    }
-//
-//}
+ package com.tms.finalproject_autoshop.controller;
+ 
+ import com.tms.finalproject_autoshop.service.EmailService;
+ import org.springframework.http.HttpStatus;
+ import org.springframework.http.ResponseEntity;
+ import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.RequestMapping;
+ import org.springframework.web.bind.annotation.RestController;
+ 
+ @RestController
+ public class EmailController {
+     private final EmailService emailService;
+ 
+     public EmailController(EmailService emailService) {
+         this.emailService = emailService;
+     }
+ 
+     @GetMapping("/notify")
+     public ResponseEntity<String> notifyEmail() {
+         emailService.sendEmail("",
+                 "",
+                 "");
+         return new ResponseEntity<>("Email sent successfully", HttpStatus.OK);
+     }
+ 
+ }
