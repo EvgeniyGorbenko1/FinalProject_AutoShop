6f445c7c105f13055e8fe83aae7cda2ee76f4628
EvgeniyGorbeko1 <evgeniy.gorbenko2@gmail.com>
2026-01-21 22:33:34 +0300

    fix annotations

--- src/main/java/com/tms/finalproject_autoshop/model/dto/AuthResponse.java
+++ src/main/java/com/tms/finalproject_autoshop/model/dto/AuthResponse.java
@@ -3,8 +3,8 @@ package com.tms.finalproject_autoshop.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
 @AllArgsConstructor
+@Data
 public class AuthResponse {
     private String jwt;
 
