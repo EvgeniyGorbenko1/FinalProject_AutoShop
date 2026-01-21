1bf5fe2a4dce8560dd8b91e12024dda3ac734f66
EvgeniyGorbeko1 <evgeniy.gorbenko2@gmail.com>
2026-01-21 22:41:16 +0300

    fix imports

--- src/main/java/com/tms/finalproject_autoshop/service/EmailService.java
+++ src/main/java/com/tms/finalproject_autoshop/service/EmailService.java
@@ -1,40 +1,39 @@
- package com.tms.finalproject_autoshop.service;
+package com.tms.finalproject_autoshop.service;
 
- import jakarta.mail.MessagingException;
- import jakarta.mail.internet.MimeMessage;
- import lombok.extern.slf4j.Slf4j;
- import org.springframework.http.HttpStatus;
- import org.springframework.http.HttpStatusCode;
- import org.springframework.http.ResponseEntity;
- import org.springframework.mail.javamail.JavaMailSender;
- import org.springframework.mail.javamail.MimeMessageHelper;
- import org.springframework.stereotype.Service;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
- @Slf4j
- @Service
- public class EmailService {
+@Slf4j
+@Service
+public class EmailService {
 
-     private final JavaMailSender mailSender;
+    private final JavaMailSender mailSender;
 
 
-     public EmailService(JavaMailSender mailSender){
-         this.mailSender = mailSender;
-     }
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
-     public ResponseEntity<HttpStatusCode> sendEmail(String to, String subject, String content){
-         MimeMessage mimeMessage = mailSender.createMimeMessage();
-         try {
-             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-             helper.setFrom("e37998274@gmail.com");
-             helper.setTo(to);
-             helper.setSubject(subject);
-             helper.setText(content, true);
-             mailSender.send(mimeMessage);
-             log.info("Email sent successfully");
-         } catch (MessagingException e){
-             log.info("Email sent failed");
+    public ResponseEntity<HttpStatusCode> sendEmail(String to, String subject, String content) {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+            helper.setFrom("${email}");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+            mailSender.send(mimeMessage);
+            log.info("Email sent successfully");
+        } catch (MessagingException e) {
+            log.info("Email sent failed");
 
-         }
-         return null;
-     }
- }
+        }
+        return null;
+    }
+}
